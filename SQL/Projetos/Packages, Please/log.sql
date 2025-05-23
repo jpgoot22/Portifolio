2726a2aab05dcf6fea06d66e1879c0e5c4b9860e
João Pedro <71711500+jpgoot22@users.noreply.github.com>
2025-05-23 12:33:19 -0300

    Update log.sql

--- SQL/Projetos/Packages, Please/log.sql
+++ SQL/Projetos/Packages, Please/log.sql
@@ -1,49 +1,58 @@
-
 -- *** The Lost Letter ***
---1
---With the provided address, I found the ID in the addresses table: id=432.
+
+-- 1
+-- Com o endereço fornecido, encontrei o ID na tabela addresses: id=432.
 SELECT "id"
 FROM "addresses"
 WHERE "address" = '900 Somerville Avenue';
 
-
---2 Using the address ID, I confirmed through the packages table whether the "Congratulatory letter" was sent and where, by checking the "contents" and "to_address_id" columns, and retrieved the package ID. (I used two queries) id=384, to_address_id=854
---2.1
-SELECT "id", "contents","to_address_id"
+-- 2
+-- Usando o ID do endereço, confirmei pela tabela packages se a "carta de felicitações" foi enviada e para onde, verificando as colunas "contents" e "to_address_id", e obtive o ID do pacote. (Usei duas consultas) id=384, to_address_id=854.
+-- 2.1
+SELECT "id", "contents", "to_address_id"
 FROM "packages"
 WHERE "from_address_id" = 432;
---2.2
-SELECT "address", "id","type"
+
+-- 2.2
+-- Verifiquei o endereço de destino usando o ID do destino (to_address_id) encontrado na etapa anterior.
+SELECT "address", "id", "type"
 FROM "addresses"
 WHERE "id" = 854;
 
-
---3 With the package ID, I checked the scans table, and the "action" column showed both a pick and a drop. The "address_id" of the drop matches the intended delivery location.
-SELECT "action", "id","address_id"
+-- 3
+-- Com o ID do pacote, consultei a tabela scans. A coluna "action" mostrou tanto um "Pick" (coleta) quanto um "Drop" (entrega). O "address_id" do Drop corresponde ao local de entrega pretendido.
+SELECT "action", "id", "address_id"
 FROM "scans"
 WHERE "package_id" = 384;
 
 -- *** The Devious Delivery ***
---1 With the provided address, I found the ID in the addresses table: id=9873
+
+-- 1
+-- Com o endereço fornecido, encontrei o ID na tabela addresses: id=9873.
 SELECT "id" FROM "addresses" WHERE "address" = '109 Tileston Street';
 
--- 2 Using the address ID, I confirmed through the packages table whether the present was sent and where, by checking the "id" and "to_address_id" columns. (I used two queries) package id=9523, to_address_id=4983
---2.1
-SELECT "id", "contents","to_address_id"
+-- 2
+-- Usando o ID do endereço, confirmei pela tabela packages se o presente foi enviado e para onde, verificando as colunas "id" e "to_address_id". (Usei duas consultas) pacote id=9523, to_address_id=4983.
+-- 2.1
+SELECT "id", "contents", "to_address_id"
 FROM "packages"
 WHERE "from_address_id" = 9873;
---2.2
-SELECT "address", "id","type"
+
+-- 2.2
+-- Verifiquei o endereço de destino usando o ID do destino encontrado.
+SELECT "address", "id", "type"
 FROM "addresses"
 WHERE "id" = 4983;
 
---3 With this formula, I was able to see that the product was picked up from the correct address, delivered to the wrong address, picked up again from the wrong address, and is now with another driver id 17.
---With the package ID, I checked the scans table, and the "action" column showed both a pick and a drop. The "address_id" of the drop matches the intended delivery location.
-SELECT "action", "driver_id","address_id", "timestamp"
+-- 3
+-- Com essa consulta, vi que o pacote foi coletado no endereço correto, entregue no endereço errado, coletado novamente do endereço errado, e está agora com outro motorista (id=17).
+SELECT "action", "driver_id", "address_id", "timestamp"
 FROM "scans"
 WHERE "package_id" = 9523;
 
--- 4 I used this code to find out the driver's name.
+-- 4
+-- Usei essa consulta para descobrir o nome do motorista atual.
 SELECT "id", "name"
 FROM "drivers"
 WHERE "id" = 17;
+
